f00576494d192e552c137400e73e0b93a0075d32
tatyborquez <niniborquez@icloud.com>
2026-09-12 13:15:09 -0300

    Implement basic Discord bot with message handling

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from generador import gen_pass
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    elif message.content.startswith('$password'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send(message.content)
